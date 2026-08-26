59d6d13e5503467e90df084bf9f66c0d659493d7
Friesi <friesi@srvb.at>
2026-08-26 23:29:34 +0200

    Enhance desktop applications installation script with additional repositories and packages

--- build_files/install/50-desktop-apps.sh
+++ build_files/install/50-desktop-apps.sh
@@ -4,11 +4,33 @@ set -euo pipefail
 
 # Additional desktop applications and packages
 
+FEDORA_VERSION="$(rpm -E '%{fedora}')"
+dnf5 config-manager addrepo --from-repofile="https://copr.fedorainfracloud.org/coprs/ublue-os/packages/repo/fedora-${FEDORA_VERSION}/ublue-os-packages-fedora-${FEDORA_VERSION}.repo"
+dnf5 config-manager setopt copr:copr.fedorainfracloud.org:ublue-os:packages.enabled=0
+dnf5 config-manager addrepo --from-repofile="https://raw.githubusercontent.com/terrapkg/subatomic-repos/main/terra.repo"
+dnf5 config-manager setopt terra.enabled=0
+
 dnf5 install -y \
+	--enablerepo=copr:copr.fedorainfracloud.org:ublue-os:packages \
+	--enablerepo=terra \
+	gamemode \
+	gnome-disk-utility \
 	kitty \
+	libFAudio \
+	libFAudio.i686 \
+	mangohud \
+	mangohud.i686 \
 	nautilus \
+	openxr \
+	steam \
+	steam-devices \
 	swappy \
+	terra-gamescope \
+	ublue-os-media-automount-udev \
 	unzip \
+	vkBasalt \
+	vkBasalt.i686 \
+	vulkan-tools \
 	wl-clipboard
 
 curl --retry 3 -fsSL https://starship.rs/install.sh | sh -s -- \
@@ -26,3 +48,5 @@ rm -f "${font_archive}"
 fc-cache -f "${font_dir}"
 
 useradd -D --shell /usr/bin/fish
+
+systemctl enable ublue-os-media-automount.service
